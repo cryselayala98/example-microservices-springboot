89c6513adf533a921ed97e44187750900297c067
cryselayala98 <criseljazminal@ufps.edu.co>
2021-10-03 18:04:09 -0500

    Se agregan validaciones en las propiedades de cada entity de product y se agregan los metodos de formateo y mostrar descripcion de errores

--- service-product/src/main/java/academy/digitallab/store/product/controller/ProductController.java
+++ service-product/src/main/java/academy/digitallab/store/product/controller/ProductController.java
@@ -3,13 +3,21 @@ package academy.digitallab.store.product.controller;
 import academy.digitallab.store.product.entity.Category;
 import academy.digitallab.store.product.entity.Product;
 import academy.digitallab.store.product.service.ProductService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/products")
@@ -52,7 +60,11 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> createProduct(@RequestBody Product product){
+    public ResponseEntity<Product> createProduct(@Valid @RequestBody Product product, BindingResult result){
+
+        if(result.hasErrors()){//validar que el cuerpo del objeto producto sea valido de acuerdo a las anotaciones y validaciones establecidas en el entity
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, this.formatMessage(result));
+        }
         Product productCreated = productService.createProduct(product);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(productCreated);
@@ -90,6 +102,31 @@ public class ProductController {
         }
 
         return ResponseEntity.ok(product);
+    }
+
+    //recibe un error, con los errores que se han ido generando (a partir de las validaciones hechas con las anotaciones del entity), se genera un flujo y se captura el flujo por medio del map, y eso se cambia por una nueva clase, un map, con el error y el msg de error
+    //revisar video 9
+    private String formatMessage(BindingResult result){
+        List<Map<String, String>> errors = result.getFieldErrors().stream()
+                .map(err ->{
+                    Map<String, String> error = new HashMap<>();
+                    error.put(err.getField(), err.getDefaultMessage());
+                    return error;
+                }).collect(Collectors.toList());
+
+        ErrorMessage errorMessage = ErrorMessage.builder()
+                .code("01")
+                .messages(errors)
+                .build();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString="";
+        try {
+            jsonString = mapper.writeValueAsString(errorMessage);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return jsonString;
     }
 
 }
--- service-product/src/main/java/academy/digitallab/store/product/entity/Product.java
+++ service-product/src/main/java/academy/digitallab/store/product/entity/Product.java
@@ -7,6 +7,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.Date;
 
 @Entity
@@ -21,16 +24,23 @@ public class Product {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty(message="El nombre no debe ser vacío")
     private String name;
+
     private String description;
+
+    @Positive(message="El stock debe ser mayor que cero")
     private Double stock;
+
     private Double price;
+
     private String status;
 
     @Column(name="created_at")
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdAt;
 
+    @NotNull(message = "La categoría no debe ser vacía")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="category_id")
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
