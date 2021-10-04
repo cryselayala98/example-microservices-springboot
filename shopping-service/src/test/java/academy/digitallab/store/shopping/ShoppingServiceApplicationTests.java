27933dc24f2ad3d99cda7ecc089de3c3d4ad0967
cryselayala98 <criseljazminal@ufps.edu.co>
2021-10-03 23:30:54 -0500

    Se agrega el servicio de compras

---
+++ shopping-service/src/test/java/academy/digitallab/store/shopping/ShoppingServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package academy.digitallab.store.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@SpringBootTest
+public class ShoppingServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
