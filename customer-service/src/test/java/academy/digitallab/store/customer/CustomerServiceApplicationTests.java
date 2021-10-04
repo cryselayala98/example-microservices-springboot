6b1e62f6740fb260028b25946981149fc80875a0
cryselayala98 <criseljazminal@ufps.edu.co>
2021-10-03 23:04:19 -0500

    Se agrega el servicio de clientes

---
+++ customer-service/src/test/java/academy/digitallab/store/customer/CustomerServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package academy.digitallab.store.customer;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CustomerServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
