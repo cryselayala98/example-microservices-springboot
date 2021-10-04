9956af134971487c14929e140778e84ce3bca3aa
cryselayala98 <criseljazminal@ufps.edu.co>
2021-10-04 08:49:12 -0500

    correccion de errores

--- config-service/src/main/java/academy/digitallab/store/config/ConfigServiceApplication.java
+++ config-service/src/main/java/academy/digitallab/store/config/ConfigServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ConfigServiceApplication.class, args);
 	}
 
