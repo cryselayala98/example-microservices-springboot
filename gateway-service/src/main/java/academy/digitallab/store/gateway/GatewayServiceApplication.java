3805ba94db033617ea47960cc4f7198a1cd9e9e2
cryselayala98 <criseljazminal@ufps.edu.co>
2021-10-05 22:47:06 -0500

    Se finaliza la configuracion del gateway service para un único punto de acceso a los microservicios

--- gateway-service/src/main/java/academy/digitallab/store/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/academy/digitallab/store/gateway/GatewayServiceApplication.java
@@ -2,7 +2,9 @@ package academy.digitallab.store.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class GatewayServiceApplication {
 
